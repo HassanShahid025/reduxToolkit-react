a386ad348a5bfaf631d443dd96ece135efc50c9e
HassanShahid025 <hassanshahid234@gmail.com>
2023-02-12 07:32:48 +0500

    decrese,increase added but not working

--- src/components/Cartitem.tsx
+++ src/components/Cartitem.tsx
@@ -1,6 +1,8 @@
 import {FaChevronUp, FaChevronDown} from 'react-icons/fa'
+import { useDispatch } from 'react-redux';
+import { removeItem, increase, decrease } from '../features/cart/cart';
 
-interface IProps {
+export interface IProps {
     id:string;
     img:string;
     title:string;
@@ -9,19 +11,24 @@ interface IProps {
 }
 
 export const Cartitem = ({id,img,title,price,amount}:IProps) => {
+    const dispatch = useDispatch();
     return(
         <article className="cart-item">
             <div>
                 <img src={img} alt="" />
                 <h4 className="item-price">${price}</h4>
-                <button className="remove-btn">remove</button>
+                <button className="remove-btn" onClick={()=>{removeItem(id)}}>remove</button>
             </div>
             <div>
-                <button className="amount-btn">
+                <button className="amount-btn" onClick={()=>{
+                    dispatch(increase(id))
+                }}>
                     <FaChevronUp/>
                 </button>
                 <p className='amount'>{amount}</p>
-                <button className="amount-btn">
+                <button className="amount-btn" onClick={()=>{
+                    dispatch(decrease(id))
+                }}>
                     <FaChevronDown/>
                 </button>
             </div>
--- src/features/cart/cart.ts
+++ src/features/cart/cart.ts
@@ -1,5 +1,6 @@
 import { createSlice} from '@reduxjs/toolkit'
 import cartItems from '../../cartItems'
+import { IProps } from '../../components/Cartitem';
 
 const initialState = {
     cartItems:cartItems,
@@ -15,9 +16,25 @@ const cartSlice = createSlice({
     reducers:{
         clearCart: (state) =>{
             state.cartItems = [];
+        },
+        removeItem: (state,action) =>{
+            const itemId = action.payload;
+            state.cartItems.filter((item)=> item.id !== itemId)
+        }, 
+        increase:(state, {payload}) => {
+            const cartItem = state.cartItems.find((item) => item.id === payload.id);
+            if(cartItem) {
+                cartItem.amount = cartItem.amount + 1;
+            }
+        },
+        decrease:(state, {payload}) => {
+            const cartItem = state.cartItems.find((item) => item.id === payload.id);
+            if(cartItem) {
+                cartItem.amount = cartItem.amount - 1;
+            }
         }
     }
 })
 
-export const {clearCart} = cartSlice.actions; 
+export const {clearCart, removeItem, increase, decrease} = cartSlice.actions; 
 export default cartSlice.reducer;
